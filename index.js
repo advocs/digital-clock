fe03301882990f1be7d380967e7b4c4e3b490cbf
aditi hr <109429321+advocs@users.noreply.github.com>
2022-08-07 12:43:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ function updateclock(){
     let m=new Date().getMinutes()
     let s=new Date().getSeconds()
     let ampm="AM"
+    
+    if(h===12)
+    {
+    ampm='PM';
+    }
+
 
     if(h>=12){
         h=h-12
